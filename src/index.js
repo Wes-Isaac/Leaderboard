569ef9452c47b48c8c0feb4d593409d208c03939
unknown <yisehakmitiku@gmail.com>
2021-11-09 04:08:42 +0800

    Add dynamic scores

--- src/index.js
+++ src/index.js
@@ -2,4 +2,12 @@ import  './main.css';
 
 const scores = document.querySelector('.scores');
 
-// scores.innerHTML = `
+scores.innerHTML = `
+<ul>
+<li>Name:100</li>
+<li class="li-grey">Name:10</li>
+<li>Name:200</li>
+<li class="li-grey">Name:40</li>
+<li>Name:60</li>
+<li class="li-grey">Name:60</li>
+</ul>`;
