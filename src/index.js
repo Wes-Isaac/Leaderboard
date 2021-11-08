2d48f26f89a0d7fd8ecbf2529f4440d701070bd2
unknown <yisehakmitiku@gmail.com>
2021-11-09 05:13:10 +0800

    Made necessary changes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,4 @@
 import './main.css';
+import index from './populate.js'
 
-const scores = document.querySelector('.scores');
-
-scores.innerHTML = `
-<ul>
-<li>Name:100</li>
-<li class="li-grey">Name:10</li>
-<li>Name:200</li>
-<li class="li-grey">Name:40</li>
-<li>Name:60</li>
-<li class="li-grey">Name:60</li>
-</ul>`;
+index();
---
+++ src/populate.js
@@ -0,0 +1,15 @@
+const index = () => {
+  const scores = document.querySelector('.scores');
+
+  scores.innerHTML = `
+  <ul>
+  <li>Name:100</li>
+  <li class="li-grey">Name:10</li>
+  <li>Name:200</li>
+  <li class="li-grey">Name:40</li>
+  <li>Name:60</li>
+  <li class="li-grey">Name:60</li>
+  </ul>`;
+};
+
+export default index;
