e9e45d9af98c9a4fc2896c0c8bf9fc9562ba94ee
unknown <yisehakmitiku@gmail.com>
2021-11-11 04:28:47 +0800

    Export functions to index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,34 @@
 import './main.css';
-import index from './populate.js';
+import * as myfun from './populate.js';
 
-index();
+const {createGame, postInfo, getData} = myfun.default;
+
+const button = document.querySelector('.submit');
+const refreshButton = document.querySelector('.refresh');
+const ul = document.querySelector('.list');
+
+createGame().then((id) => {
+  
+  button.addEventListener('click',(e) => {
+    const name = document.querySelector('.name').value;
+    const score = document.querySelector('.score').value;
+    if (name && score) {
+      e.preventDefault();
+      postInfo(id,name,score);
+      document.querySelector('form').reset();
+    }
+  });
+
+  refreshButton.addEventListener('click', () => {
+    getData(id).then((result) => {
+      ul.innerHTML = '';
+      result.forEach((res, index) => {
+        console.log(res);
+        index % 2 === 0 ? ul.innerHTML += `<li>${res.user}:${res.score}</li>`: ul.innerHTML += `<li  class="li-grey">${res.user}:${res.score}</li>`;
+        
+      })
+    })
+  })
+
+
+});
--- src/populate.js
+++ src/populate.js
@@ -9,9 +9,6 @@ async function createGame(){
   return id;
 }
 
-const button = document.querySelector('.submit');
-const refreshButton = document.querySelector('.refresh');
-const ul = document.querySelector('.list');
 
 async function postInfo(id, name, score) {
  const info = await axios.post(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${id}/scores`, {
@@ -29,32 +26,30 @@ async function getData(id) {
 }
 
 
-createGame().then((id) => {
+// createGame().then((id) => {
   
-  button.addEventListener('click',(e) => {
-    const name = document.querySelector('.name').value;
-    const score = document.querySelector('.score').value;
-    if (name && score) {
-      e.preventDefault();
-      postInfo(id,name,score);
-      document.querySelector('form').reset();
-    }
-  });
+//   button.addEventListener('click',(e) => {
+//     const name = document.querySelector('.name').value;
+//     const score = document.querySelector('.score').value;
+//     if (name && score) {
+//       e.preventDefault();
+//       postInfo(id,name,score);
+//       document.querySelector('form').reset();
+//     }
+//   });
 
-  refreshButton.addEventListener('click', () => {
-    getData(id).then((result) => {
-      ul.innerHTML = '';
-      result.forEach((res, index) => {
-        console.log(res);
-        index % 2 === 0 ? ul.innerHTML += `<li>${res.user}:${res.score}</li>`: ul.innerHTML += `<li  class="li-grey">${res.user}:${res.score}</li>`;
+//   refreshButton.addEventListener('click', () => {
+//     getData(id).then((result) => {
+//       ul.innerHTML = '';
+//       result.forEach((res, index) => {
+//         console.log(res);
+//         index % 2 === 0 ? ul.innerHTML += `<li>${res.user}:${res.score}</li>`: ul.innerHTML += `<li  class="li-grey">${res.user}:${res.score}</li>`;
         
-      })
-    })
-  })
+//       })
+//     })
+//   })
 
 
-})
+// })
 
-
-
-
+export default {createGame, postInfo, getData};
