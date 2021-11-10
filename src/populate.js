0682132b3bea2aa9976adb840ba47e8b6a39b328
unknown <yisehakmitiku@gmail.com>
2021-11-11 04:29:48 +0800

    Remove export functions from populate.js

--- src/populate.js
+++ src/populate.js
@@ -25,31 +25,4 @@ async function getData(id) {
   return res.data.result;
 }
 
-
-// createGame().then((id) => {
-  
-//   button.addEventListener('click',(e) => {
-//     const name = document.querySelector('.name').value;
-//     const score = document.querySelector('.score').value;
-//     if (name && score) {
-//       e.preventDefault();
-//       postInfo(id,name,score);
-//       document.querySelector('form').reset();
-//     }
-//   });
-
-//   refreshButton.addEventListener('click', () => {
-//     getData(id).then((result) => {
-//       ul.innerHTML = '';
-//       result.forEach((res, index) => {
-//         console.log(res);
-//         index % 2 === 0 ? ul.innerHTML += `<li>${res.user}:${res.score}</li>`: ul.innerHTML += `<li  class="li-grey">${res.user}:${res.score}</li>`;
-        
-//       })
-//     })
-//   })
-
-
-// })
-
 export default {createGame, postInfo, getData};
