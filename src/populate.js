23003b935c506a17555da8eb51e0da3b42e3ff3d
unknown <yisehakmitiku@gmail.com>
2021-11-11 04:14:03 +0800

    Remove previous unwanted methods

--- src/populate.js
+++ src/populate.js
@@ -58,18 +58,3 @@ createGame().then((id) => {
 
 
 
-const index = () => {
-  // const scores = document.querySelector('.scores');
-
-  // scores.innerHTML = `
-  // <ul>
-  // <li>Name:100</li>
-  // <li class="li-grey">Name:10</li>
-  // <li>Name:200</li>
-  // <li class="li-grey">Name:40</li>
-  // <li>Name:60</li>
-  // <li class="li-grey">Name:60</li>
-  // </ul>`;
-};
-
-export default index;
