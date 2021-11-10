b881a56e5124d2befc583a96b6628c6337f89401
unknown <yisehakmitiku@gmail.com>
2021-11-11 04:09:09 +0800

    Implement createGame

--- src/populate.js
+++ src/populate.js
@@ -1,15 +1,75 @@
-const index = () => {
-  const scores = document.querySelector('.scores');
+// const axios = require('axios');
+import axios from 'axios';
 
-  scores.innerHTML = `
-  <ul>
-  <li>Name:100</li>
-  <li class="li-grey">Name:10</li>
-  <li>Name:200</li>
-  <li class="li-grey">Name:40</li>
-  <li>Name:60</li>
-  <li class="li-grey">Name:60</li>
-  </ul>`;
+async function createGame(){
+  const response = await axios.post('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
+      name: "my cool game"});
+  const myArray = response.data.result.split(":");
+  const id = myArray[1].split(' ')[1];
+  return id;
+}
+
+// const button = document.querySelector('.submit');
+// const refreshButton = document.querySelector('.refresh');
+// const ul = document.querySelector('.list');
+
+// async function postInfo(id, name, score) {
+//  const info = await axios.post(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${id}/scores`, {
+//     user: name,
+//     score: score
+//   });
+//   console.log(info.data.result);
+
+// }
+
+// async function getData(id) {
+//   const res = await axios.get(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${id}/scores`);
+
+//   return res.data.result;
+// }
+
+
+// createGame().then((id) => {
+  
+//   button.addEventListener('click',(e) => {
+//     const name = document.querySelector('.name').value;
+//     const score = document.querySelector('.score').value;
+//     if (name && score) {
+//       e.preventDefault();
+//       postInfo(id,name,score);
+//       document.querySelector('form').reset();
+//     }
+//   });
+
+//   refreshButton.addEventListener('click', () => {
+//     getData(id).then((result) => {
+//       ul.innerHTML = '';
+//       result.forEach((res, index) => {
+//         console.log(res);
+//         index % 2 === 0 ? ul.innerHTML += `<li>${res.user}:${res.score}</li>`: ul.innerHTML += `<li  class="li-grey">${res.user}:${res.score}</li>`;
+        
+//       })
+//     })
+//   })
+
+
+// })
+
+
+
+
+const index = () => {
+  // const scores = document.querySelector('.scores');
+
+  // scores.innerHTML = `
+  // <ul>
+  // <li>Name:100</li>
+  // <li class="li-grey">Name:10</li>
+  // <li>Name:200</li>
+  // <li class="li-grey">Name:40</li>
+  // <li>Name:60</li>
+  // <li class="li-grey">Name:60</li>
+  // </ul>`;
 };
 
 export default index;
