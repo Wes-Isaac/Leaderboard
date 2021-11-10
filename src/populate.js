8f0deaf17c9c24a750190f358ca3ec60a20d1e2d
unknown <yisehakmitiku@gmail.com>
2021-11-11 05:24:26 +0800

    Fix linter errors

--- src/populate.js
+++ src/populate.js
@@ -5,19 +5,19 @@ const createGame = async () => {
   const myArray = response.data.result.split(':');
   const id = myArray[1].split(' ')[1];
   return id;
-}
+};
 
 const postInfo = async (id, name, val) => {
   await axios.post(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${id}/scores`, {
     user: name,
     score: val,
   });
-}
+};
 
-const getData= async (id) => {
+const getData = async (id) => {
   const res = await axios.get(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${id}/scores`);
 
   return res.data.result;
-}
+};
 
 export default { createGame, postInfo, getData };
