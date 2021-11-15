4902bdfb8bf8472fb41499c5567a75b7219e68a8
unknown <yisehakmitiku@gmail.com>
2021-11-16 05:31:43 +0800

    Update post api

--- src/index.js
+++ src/index.js
@@ -1,28 +1,31 @@
 import './main.css';
 import * as myfun from './populate.js';
 
-const { createGame, postInfo, getData } = myfun.default;
+const { postInfo, getData } = myfun.default;
 const button = document.querySelector('.submit');
 const refreshButton = document.querySelector('.refresh');
 const ul = document.querySelector('.list');
 
-createGame().then((id) => {
-  button.addEventListener('click', (e) => {
-    const name = document.querySelector('.name').value;
-    const score = document.querySelector('.score').value;
-    if (name && score) {
-      e.preventDefault();
-      postInfo(id, name, score);
-      document.querySelector('form').reset();
-    }
-  });
-
-  refreshButton.addEventListener('click', () => {
-    getData(id).then((result) => {
-      ul.innerHTML = '';
-      result.forEach((res) => {
-        ul.innerHTML += `<li  class="li-grey">${res.user} : ${res.score}</li>`;
-      });
+const render = () => {
+  getData().then((result) => {
+    ul.innerHTML = '';
+    result.forEach((res) => {
+      ul.innerHTML += `<li  class="li-grey">${res.user} : ${res.score}</li>`;
     });
   });
+};
+
+button.addEventListener('click', (e) => {
+  const name = document.querySelector('.name').value;
+  const score = document.querySelector('.score').value;
+  if (name && score) {
+    e.preventDefault();
+    postInfo(name, score);
+    document.querySelector('form').reset();
+    render();
+  }
 });
+
+render();
+
+refreshButton.addEventListener('click', render);
--- src/populate.js
+++ src/populate.js
@@ -1,23 +1,16 @@
 import axios from 'axios';
 
-const createGame = async () => {
-  const response = await axios.post('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', { name: 'my cool game' });
-  const myArray = response.data.result.split(':');
-  const id = myArray[1].split(' ')[1];
-  return id;
-};
-
-const postInfo = async (id, name, val) => {
-  await axios.post(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${id}/scores`, {
+const postInfo = async (name, val) => {
+  await axios.post('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/8mWORz68pdvNK6avThO2/scores', {
     user: name,
     score: val,
   });
 };
 
-const getData = async (id) => {
-  const res = await axios.get(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${id}/scores`);
+const getData = async () => {
+  const res = await axios.get('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/8mWORz68pdvNK6avThO2/scores');
 
   return res.data.result;
 };
 
-export default { createGame, postInfo, getData };
+export default { postInfo, getData };
